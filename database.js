a406dbd1684238b33c157688f2388772ae7430f2
oklyukvin <82665350+oklyukvin@users.noreply.github.com>
2021-07-15 17:22:57 +0300

    homework-1

---
+++ database.js
@@ -0,0 +1,10 @@
+const goods = [
+    { title: 'Adidas', price: 72.00 },
+    { title: 'Reebok', price: 152.00 },
+    { title: 'New Balance', price: 223.00 },
+    { title: 'Nike T-shirt', price: 42.00 },
+    { title: 'Adidas', price: 72.00 },
+    { title: 'Reebok', price: 152.00 },
+    { title: 'New Balance', price: 223.00 },
+    { title: 'Nike T-shirt', price: 42.00 },
+];
